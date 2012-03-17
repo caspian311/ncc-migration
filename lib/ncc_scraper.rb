684771bc204622d9a9615401b6b23cd76687ed1e
matt.todd <matt.todd@asolutions.com>
2012-03-17 08:28:54 -0500

    getting the pagination links

--- lib/ncc_scraper.rb
+++ lib/ncc_scraper.rb
@@ -1,4 +1,18 @@
+require "capybara/rspec"
+
+include Capybara::DSL
+
+Capybara.current_driver = :selenium 
+Capybara.app_host = "http://www.ncc-stl.org/"
+Capybara.run_server = false 
+Capybara.default_selector = :css 
+
 class NccScraper
    def scrape_site
+      visit "/site/cpage.asp?sec_id=1673&cpage_id=1982"
+
+      all("a.page").each do |page_link|
+         puts page_link[:href]
+      end
    end
 end
